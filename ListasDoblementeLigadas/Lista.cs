d6bba218f53206db9835214ed8eaa5436fd4dc95
Sergio Armando Cazares Ruiz <Cazares250@outlook.com>
2022-04-23 14:06:12 -0700

    Creando Listas

---
+++ ListasDoblementeLigadas/Lista.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasDoblementeLigadas
+{
+    public class Lista
+    {
+        Nodo nodoAnterior;
+        Nodo nodoInicial;
+        Nodo nodoSiguiente;
+
+        public Lista()
+        {
+            nodoInicial = new Nodo();
+        }
+        
+    }
+}
